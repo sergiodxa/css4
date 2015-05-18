293a4ee6f3855e910e742019ba6ab6a7aa063c63
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-18 02:37:39 -0300

    iniciado proyecto

---
+++ README.md
@@ -0,0 +1,27 @@
+# CSS4
+
+## Build
+Para generar los archivos minificados hay que instalar Gulp y sus plugins con:
+
+```
+[sudo] npm i
+```
+
+Luego hay que iniciar gulp con:
+
+```
+gulp
+```
+
+## Estructura de archivos
+Los archivos se encuentran en la carpeta *./src*, dentro de esta se encuentran las carpetas:
+
+* css
+* html
+* js
+
+En *css* se encuentran los estilos del sitio y de la librería prism.
+
+En *html* se encuentran el index.html y el gh-fork-ribbon.html sin minificar.
+
+En *js* se encuentra el archivo principal donde esta el código de analytics y el uso del import de gh-fork-ribbon.
---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+(function () {
+  $('h2').on('click', function (e) {
+    e.preventDefault();
+
+    location.hash = $(this).attr('id');
+
+    markActive();
+  });
+
+  // show github fork ribbon
+  var ribbon = document.querySelector('#gh-fork-ribbon').import.querySelector('.github-fork-ribbon-wrapper');
+  $(ribbon).appendTo('body');
+
+  // google analytics code
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48432002-8', 'auto');
+  ga('send', 'pageview');
+
+  function markActive() {
+    var hash = location.hash;
+
+    if (hash) {
+      $('h2').removeClass('active');
+      $(hash).addClass('active');
+    }
+  }
+
+  markActive();
+
+})();
