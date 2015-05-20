a33805ac3e526a2d437bfaee4ca46375ac8176e9
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-20 01:40:42 -0300

    analytics, ribbon de en proceso y link de secciones colocados

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,4 @@
 (function () {
-  $('h2').on('click', function (e) {
-    e.preventDefault();
-
-    location.hash = $(this).attr('id');
-
-    markActive();
-  });
-
-  // show github fork ribbon
-  var ribbon = document.querySelector('#gh-fork-ribbon').import.querySelector('.github-fork-ribbon-wrapper');
-  $(ribbon).appendTo('body');
-
-  // google analytics code
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-48432002-8', 'auto');
-  ga('send', 'pageview');
-
   function markActive() {
     var hash = location.hash;
 
@@ -30,5 +9,30 @@
   }
 
   markActive();
+
+  $('h2').on('click', function (e) {
+    e.preventDefault();
+
+    location.hash = $(this).attr('id');
+
+    markActive();
+  });
+
+  // show github fork ribbon
+  var ribbonImport = document.querySelector('#gh-fork-ribbon').import;
+  var leftBottom   = ribbonImport.querySelector('.github-fork-ribbon-wrapper.left-bottom');
+  var rightBottom  = ribbonImport.querySelector('.github-fork-ribbon-wrapper.right-bottom');
+
+  $(leftBottom).appendTo('body');
+  $(rightBottom).appendTo('body');
+
+  // google analytics code
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48432002-3', 'auto');
+  ga('send', 'pageview');
 
 })();
